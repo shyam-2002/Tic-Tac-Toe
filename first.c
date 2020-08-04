21da9ada403e545332d49ee93117609a659d6f92
Shyam Sundar Goyal <goyalshyamsundar2@gmail.com>
2020-08-04 18:52:48 +0530

    input array check done

--- first.c
+++ first.c
@@ -2,16 +2,50 @@
 char player_one_char,player_two_char;
 int count=1;
 int user_index,game_over = 0;
-void scan_and_print(){
 char input_arr[3][3];
+char winning_char ;
+int check_game_status(){
+if(count <=10){
+for(int i=0;i<3;i++){
+  if(input_arr[i][0] == input_arr[i][1] && input_arr[i][1]== input_arr[i][2] ){
+    winning_char = input_arr[i][0];
+    return 1;}
+  }
+for(int j=0;j<3;j++){
+if(input_arr[0][j] == input_arr[1][j] && input_arr[1][j]== input_arr[2][j] ){
+    winning_char = input_arr[0][j];
+    return 1;
+  }  
+ } 
+if(input_arr[0][0] == input_arr[1][1] && input_arr[1][1] == input_arr[2][2]){
+  winning_char = input_arr[0][0];
+  return 1;}
+if(input_arr[2][2] == input_arr[1][1] && input_arr[1][1] == input_arr[2][2]){
+  winning_char = input_arr[1][1];
+  return 1;}
+if(count == 10){
+winning_char = 'd';
+return 1;}
+return 0;   
+}
+
+}
+void scan_and_print(){
+
 while(game_over != 1){
 printf("Choose the index from below where you want to put %c",(count%2 == 0)?player_two_char:player_one_char);
 
 printf("%d | %d | %d\n_________\n%d | %d | %d\n_________\n%d | %d | %d",1,2,3,4,5,6,7,8,9);
 scanf("%d",&player_input_index);
 *(input_arr+player_input_index-1) = (count%2 ==0)?player_two_char:player_one_char;
+ game_over = check_game_status();
  
   }
+if(winning_char == player_one_char){
+ printf("Player 1 is winner");}
+else if(winning_char = player_two_char){
+ printf("Player 2 is winner");}
+else printf("Match is drawn");    
 }
 void gaming_setup(){
 
