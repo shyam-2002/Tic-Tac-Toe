b0c0ec94a9a6d7f3207ffeaa554c9d754cd364ea
Shyam Sundar Goyal <goyalshyamsundar2@gmail.com>
2020-08-04 18:29:03 +0530

    taken user input for play but not checked

--- first.c
+++ first.c
@@ -1,6 +1,18 @@
 #include<stdio.h>
 char player_one_char,player_two_char;
-int count=0;
+int count=1;
+int user_index,game_over = 0;
+void scan_and_print(){
+char input_arr[3][3];
+while(game_over != 1){
+printf("Choose the index from below where you want to put %c",(count%2 == 0)?player_two_char:player_one_char);
+
+printf("%d | %d | %d\n_________\n%d | %d | %d\n_________\n%d | %d | %d",1,2,3,4,5,6,7,8,9);
+scanf("%d",&player_input_index);
+*(input_arr+player_input_index-1) = (count%2 ==0)?player_two_char:player_one_char;
+ 
+  }
+}
 void gaming_setup(){
 
 printf("Player 1 :(enter one of either o or x)");
@@ -9,12 +21,9 @@ if(player_one_char == 'o'){
 player_two_char = 'x';
 }
 else player_two_char = 'o';
-while(game_over != 1){
 
-printf("Choose the index from below where you want to put %c",(count%2 == 0)?player_two_char:player_one_char);
-count++;
-}
-printf("%d | %d | %d\n______\n%d | %d | %d\n______\n%d | %d | %d",1,2,3,4,5,6,7,8,9);
+
+
 
 }
 
@@ -28,6 +37,7 @@ else printf("Welcome back\n");
 
 int main(){
 int gaming_count = 0;
-
+greet(gaming_count);
+gaming_setup();
 
 }
